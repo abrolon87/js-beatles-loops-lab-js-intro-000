b47ca2d4aaaae9a5c490f055c7a52f0d30e16471
Amanda R <abrolon87@gmail.com>
2019-08-17 03:43:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-}// add solution here
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + 'plays' + instruments[i])
+  }
+  return x;
+ }// add solution here
 
 function johnLennonFacts(array) {
 
