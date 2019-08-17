91b8e435bf0d09762dea4fb3eff9fad7e96795ed
Amanda R <abrolon87@gmail.com>
2019-08-17 03:56:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + ' plays ' + instruments[i])
+    array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
  }// add solution here
 
 function johnLennonFacts(array) {
   while (array > 0) {
-    console.log(array +`!!!`);
+    array.push(array +`!!!`);
   }
   return array;
 }
